b09595dd02f163d035b48502dd87757483913329
qq1256807020 <1256807020@qq.com>
2018-10-26 12:48:21 +0800

    demo

---
+++ src/pages/demo/Life.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import Child from './Child'
+import { Button, Input } from 'antd'
+import './index.less'
+class Life extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      title: '生命周期函数',
+      count: 0
+    }
+  }
+  handleAdd = () => {
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
+  render () {
+    let style = {
+      padding: 50
+    }
+    return (
+      < div className='content'>
+        <Input></Input>
+        <Button onClick={this.handleAdd} type="primary">AntD点击一下</Button>
+        <button onClick={this.handleAdd}>点击一下</button>
+        <p>{this.state.title}</p>
+        <p>{this.state.count}</p>
+        <Child name={this.state.count}></Child>
+      </div >
+    )
+  }
+}
+export default Life
