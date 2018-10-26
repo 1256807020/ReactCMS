e6c90212ce399fab8e18c01cb859183afd8063a9
qq1256807020 <1256807020@qq.com>
2018-10-26 18:24:09 +0800

    nav-left menu

--- src/components/NavLeft/index.js
+++ src/components/NavLeft/index.js
@@ -1,4 +1,9 @@
 import React from 'react'
+import { Menu, Icon } from 'antd';
+import './index.less'
+import MenuConfig from '../../config/menuConfig'
+// 注意MenuConfig的位置
+const SubMenu = Menu.SubMenu;
 class NavLeft extends React.Component {
   constructor(props) {
     super(props)
@@ -10,7 +15,20 @@ class NavLeft extends React.Component {
     // }
     return (
       // <div style={style}>NavLeft</div>
-      <div>NavLeft</div>
+      <div>
+        <div className="logo">
+          <img src="/assets/logo-ant.svg" alt="" />
+          <h1>React CMS</h1>
+        </div>
+        <Menu theme='dark'>
+          <SubMenu key="sub1" title={<span><Icon type="mail" /><span>Navigation One</span></span>}>
+            <Menu.Item key="1">Option 1</Menu.Item>
+            <Menu.Item key="2">Option 2</Menu.Item>
+            <Menu.Item key="3">Option 3</Menu.Item>
+            <Menu.Item key="4">Option 4</Menu.Item>
+          </SubMenu>
+        </Menu>
+      </div>
     )
   }
 }
