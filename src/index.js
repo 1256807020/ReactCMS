9354d817e08ef18c7a119ca5fd74467a1aa8c2ca
qq1256807020 <1256807020@qq.com>
2018-10-26 21:52:09 +0800

    react-router-dom基本用法

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Admin from './Admin';
+// import Admin from './Admin';
+import Home from './pages/route_demo/route1/Home'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Admin />, document.getElementById('root'));
+ReactDOM.render(<Home />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
