b61d1d8d64e5d78a3afbef0bea3fdae558e4f56f
qq1256807020 <1256807020@qq.com>
2018-10-27 11:11:43 +0800

    动态路由demo

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import Admin from './Admin';
-import Router from './pages/route_demo/route2/router'
+import Router from './pages/route_demo/route3/router'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Router />, document.getElementById('root'));
