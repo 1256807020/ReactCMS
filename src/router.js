3db7dc51b21b00eca823d9bb4fcca20d81f63fb5
qq1256807020 <1256807020@qq.com>
2018-10-28 19:02:12 +0800

    ui组件

--- src/router.js
+++ src/router.js
@@ -4,7 +4,14 @@ import App from './App'
 import Login from './pages/login'
 import Admin from './Admin'
 import Buttons from './pages/ui/button'
+import Modals from './pages/ui/modals'
 import NoMatch from './pages/nomatch'
+import Loadings from './pages/ui/loadings'
+import Notice from './pages/ui/notice'
+import Messages from './pages/ui/messages'
+import Tabs from './pages/ui/tabs'
+import Gallery from './pages/ui/gallery'
+import Carousels from './pages/ui/carousel'
 export default class IRouter extends React.Component {
   constructor(props) {
     super(props)
@@ -26,14 +33,21 @@ export default class IRouter extends React.Component {
             // </Admin>
             <Admin>
               <Switch>
-              <Route path="/admin/ui/buttons" component={Buttons} />
-              <Route component={NoMatch} />
-              </Switch>              
+                <Route path="/admin/ui/buttons" component={Buttons} />
+                <Route path="/admin/ui/modals" component={Modals} />
+                <Route path="/admin/ui/loadings" component={Loadings} />
+                <Route path="/admin/ui/notification" component={Notice} />
+                <Route path="/admin/ui/messages" component={Messages} />
+                <Route path="/admin/ui/tabs" component={Tabs} />
+                <Route path="/admin/ui/gallery" component={Gallery} />
+                <Route path="/admin/ui/carousel" component={Carousels} />
+                <Route component={NoMatch} />
+              </Switch>
             </Admin>
           } />
 
           <Route path='/order/detail' component={Login} />
-          
+
         </App>
 
       </HashRouter>
